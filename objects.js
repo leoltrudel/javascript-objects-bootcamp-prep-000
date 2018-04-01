9e1dd45bb77582adc75cef0866bb8c99733f89f3
Leo Trudel <leoltrudel@gmail.com>
2018-04-01 17:46:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {artistName: "song"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artist] = songTitle
+  playlist[artistName] = songTitle
     return playlist
 }
